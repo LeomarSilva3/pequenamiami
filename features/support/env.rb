cbdf6283f82128b4ce110f0f9fa975758925fdef
Leomar da Silva Barbosa <leomar.barbosa@kaiostech.com>
2019-09-02 14:58:52 -0400

    update

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,10 +5,12 @@ require 'selenium-webdriver'
 require 'report_builder'
 require 'allure-cucumber'
 
+CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["ENV_TYPE"]}.yaml"))
+
 Capybara.configure do |config|
     config.default_driver = :selenium_chrome#_headless
     config.default_max_wait_time = 20
-    config.app_host = 'http://www.pequenamiami.com.br'
+    config.app_host = CONFIG["url"]
     
     
 end
