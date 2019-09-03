813c40360fedfea20023729dee59a450a43a697b
Leomar da Silva Barbosa <leomar.barbosa@kaiostech.com>
2019-09-03 11:59:41 -0400

    update

--- features/support/env.rb
+++ features/support/env.rb
@@ -15,6 +15,15 @@ CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["ENV_T
     when "chrome"
         @driver = :selenium_chrome
     when "headless"
+        Capybara.register_driver :selenium_headless do |app|
+            chrome_options = Selenium::WebDriver::Chrome::OPtions.new.tap do |options|
+                options.add_argument "--headless"
+                options.add_argument "--disable-gpu"
+                options.add_argument "--no-sandbox"
+                options.add_argument "--disable-site-isolation-trials"
+            end
+            Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
+        end
         @driver = :selenium_chrome_headless
     else
         puts "Invalid Browser"
@@ -24,4 +33,4 @@ Capybara.configure do |config|
   config.default_driver = @driver#:selenium_chrome#_headless
   config.default_max_wait_time = 20
   config.app_host = CONFIG["url"]    
-end
+end 
