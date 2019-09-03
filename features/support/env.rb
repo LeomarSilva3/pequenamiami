5dd0af724853ccf1ffc922c9cf3999670c5c8eee
Leomar da Silva Barbosa <leomar.barbosa@kaiostech.com>
2019-09-03 11:22:14 -0400

    update

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,19 +9,19 @@ require 'allure-cucumber'
 CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["ENV_TYPE"]}.yaml"))
 
 #configuração para execução de browsers
-case ENV["BROWSER"]
-when "firefox"
-    @driver = :selenium
-when "chrome"
-    @driver = :selenium_chrome
-when "headless"
-    @driver = :selenium_chrome_headless
-begin
-    puts "Invalid Browser"
-end        
+    case ENV["BROWSER"]
+    when "firefox"
+        @driver = :selenium
+    when "chrome"
+        @driver = :selenium_chrome
+    when "headless"
+        @driver = :selenium_chrome_headless
+    else
+        puts "Invalid Browser"
+    end        
 
 Capybara.configure do |config|
-    config.default_driver = @driver#:selenium_chrome#_headless
-    config.default_max_wait_time = 20
-    config.app_host = CONFIG["url"]    
+  config.default_driver = @driver#:selenium_chrome#_headless
+  config.default_max_wait_time = 20
+  config.app_host = CONFIG["url"]    
 end
